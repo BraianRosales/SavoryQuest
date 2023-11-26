f95d948584380b641ab94350eb1076d453dfc9a7
Rosales <braian.rosales@accusys.com.ar>
2023-11-26 00:29:15 -0300

    i add angular material + mat-icons + header layout

--- src/app/core/components/components.module.ts
+++ src/app/core/components/components.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 @NgModule({
   declarations: [HeaderComponent, SidebarComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, SharedModule],
   exports: [HeaderComponent, SidebarComponent],
 })
 export class ComponentsModule { }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
-
+import { MaterialModule } from '../@material/material.module';
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [MaterialModule, CommonModule],
+  exports: [MaterialModule],
 })
-export class SharedModule { }
+export class SharedModule {}
